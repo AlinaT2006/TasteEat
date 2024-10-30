96e8b336fa1439612add7631774ce03ccac265c1
Alina <alinatuhtaeva@gmail.com>
2024-10-30 22:18:16 +0500

    testimonials done

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navToggle = document.querySelector(".header__nav-toggle");
-    const navList = document.querySelector(".header__nav-list");
-  
-    navToggle.addEventListener("click", () => {
-      navList.classList.toggle("show");
-    });
+  const navToggle = document.querySelector(".header__nav-toggle");
+  const navList = document.querySelector(".header__nav-list");
+
+  navToggle.addEventListener("click", () => {
+    navList.classList.toggle("show");
   });
-  
+});
+
+
+
+
+
